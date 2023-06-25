c7931917e2b2faba00ed102d1ba8fb295b8305fd
EoinKanro <ivanros.nh@gmail.com>
2023-06-26 01:26:06 +0400

    Javadoc

--- src/main/java/io/github/eoinkanro/commons/mvc/impl/StayAction.java
+++ src/main/java/io/github/eoinkanro/commons/mvc/impl/StayAction.java
@@ -2,5 +2,8 @@ package io.github.eoinkanro.commons.mvc.impl;
 
 import io.github.eoinkanro.commons.mvc.Action;
 
+/**
+ * Perform stay in current controller
+ */
 public class StayAction implements Action {
 }
